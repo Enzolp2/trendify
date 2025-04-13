b065dea634b61e0dd896abff7ab81829d3d70f5f
Enzo Lemos <enzolp2@hotmail.com>
2025-04-13 17:22:42 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:5173', // TODO: add env variable to this
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
